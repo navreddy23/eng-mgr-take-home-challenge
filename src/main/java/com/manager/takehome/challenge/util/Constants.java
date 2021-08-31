d7c36bd6af4d661509ffff6ace116db85b3bb203
navin reddy <navin.x.reddy@disney.com>
2021-08-31 12:49:02 -0500

    adding more integrations tests for worked hours

--- src/main/java/com/manager/takehome/challenge/util/Constants.java
+++ src/main/java/com/manager/takehome/challenge/util/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
   public static final String POST_HOURS_SUCCESS = "worked hours posted successfully";
   public static final String POST_HOURS_FAILED = "posting worked hours failed";
   public static final String NO_USER = "user does not exist in the system";
-  public static final String HOURS_ALREADY_RECORDED = "user has already recorded "
+  public static final String HOURS_ALREADY_RECORDED = "user has already recorded"
       + "worked hours for this day";
 }
